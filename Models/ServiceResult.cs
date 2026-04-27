3d0515d39444d0063ab708353787d6324865729c
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-28 00:36:21 +0700

    FEAT: Add new return object for service
    Service will return consistent object to check for successful operation

---
+++ Models/ServiceResult.cs
@@ -0,0 +1,12 @@
+﻿namespace lievee.Models
+{
+    public class ServiceResult<T>
+    {
+        public bool IsSuccess { get; private set; }
+        public T? Data { get; private set; }
+        public string? Err { get; private set; }
+
+        public static ServiceResult<T> Success(T _data) => new ServiceResult<T> { IsSuccess = true, Data = _data };
+        public static ServiceResult<T> Failed(string msg) => new ServiceResult<T> { IsSuccess = false, Err = msg };
+    }
+}
