0b6f49c054c8a4cc8206a3371fa7cdc1b887e28f
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-27 17:44:55 +0700

    FIX: Add service layer for registration
    Previously the controller access the repository interface directly

--- Controllers/RegistrationController.cs
+++ Controllers/RegistrationController.cs
@@ -1,6 +1,35 @@
-﻿namespace lievee.Controllers
+﻿using lievee.Models;
+using lievee.Repositories;
+using lievee.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lievee.Controllers
 {
-    public class RegistrationController
+    [ApiController]
+    [Route("[controller]")]
+    public class RegistrationController : ControllerBase
     {
+        private readonly IRegistrationService _service;
+        public RegistrationController(IRegistrationService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetRegisteredVisitors(DateOnly? startDate, DateOnly? endDate)
+        {
+            var today = DateOnly.FromDateTime(DateTime.Today);
+
+            if (startDate > endDate)
+            {
+                return BadRequest(new
+                {
+                    message = "start date cannot be later than end date"
+                });
+            }
+
+            var visitors = _service.GetRegisteredVisitors(startDate ?? today, endDate ?? today);
+            return Ok(visitors);
+        }
     }
 }
