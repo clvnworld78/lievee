5abc33db479a1e74602dc7a82745f87d42a4419a
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-28 01:13:52 +0700

    FEAT: Add controller for creating a unique link

---
+++ Controllers/RegistrationLinkController.cs
@@ -0,0 +1,35 @@
+﻿using lievee.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lievee.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegistrationLinkController : ControllerBase
+    {
+        private readonly IUniqueCodeService _service;
+        public RegistrationLinkController(IUniqueCodeService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GenerateUniqueLink()
+        {
+            var svc = await _service.GenerateUniqueLinkAsync();
+            if (!svc.IsSuccess)
+            {
+                return StatusCode(500, new
+                {
+                    message = svc.Err
+                });
+            }
+
+            return Ok(new
+            {
+                message = "link successfully generated",
+                code = svc.Data
+            });
+        }
+    }
+}
