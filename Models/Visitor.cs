8f83dfb2037e7e32012a7442c73fa9f3a754d69c
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-28 00:31:32 +0700

    REFRACTOR: DTO is renamed to UniqueCode
    Because not needed anymore

--- Models/Visitor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace lievee.Models
-{
-    public class Visitor
-    {
-        public int Id { get; set; }
-        public required string Name { get; set; }
-        public required int PhoneNumber { get; set; }
-    }
-}
