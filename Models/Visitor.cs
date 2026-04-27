4aeb2d214ff77b555a12ca601a20529f94ba96d9
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-27 17:46:33 +0700

    FEAT: Add visitor DTO model

--- Models/Visitor.cs
+++ Models/Visitor.cs
@@ -2,5 +2,8 @@
 {
     public class Visitor
     {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required int PhoneNumber { get; set; }
     }
 }
