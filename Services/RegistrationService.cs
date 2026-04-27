14ec949921cfaef93b8e94d02c8c3db3944630d7
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-27 17:49:46 +0700

    FEAT: Add concrete implementation for the registration service interface

---
+++ Services/RegistrationService.cs
@@ -0,0 +1,19 @@
+﻿using lievee.Models;
+using lievee.Repositories;
+
+namespace lievee.Services
+{
+    public class RegistrationService : IRegistrationService
+    {
+        private readonly IRegistrationRepository _repo;
+        public RegistrationService(IRegistrationRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public List<RegisteredVisitor> GetRegisteredVisitors(DateOnly startDate, DateOnly endDate)
+        {
+            return _repo.GetRegisteredVisitors(startDate, endDate);
+        }
+    }
+}
