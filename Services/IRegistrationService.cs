9d61da763cd2e569cf5d62c52828f6375338ab6c
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-27 17:49:12 +0700

    FEAT: Add interface for registration service layer

---
+++ Services/IRegistrationService.cs
@@ -0,0 +1,9 @@
+﻿using lievee.Models;
+
+namespace lievee.Services
+{
+    public interface IRegistrationService
+    {
+        List<RegisteredVisitor> GetRegisteredVisitors(DateOnly startDate, DateOnly endDate);
+    }
+}
