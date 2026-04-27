fb77337e2f09b06daab846798782be0ac8e37dee
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-27 17:48:27 +0700

    FEAT: Add concrete class that implement the repo interface

--- Repositories/RegistrationRepository.cs
+++ Repositories/RegistrationRepository.cs
@@ -1,6 +1,38 @@
-﻿namespace lievee.Repositories
+﻿using lievee.Database;
+using lievee.Models;
+using Npgsql;
+
+namespace lievee.Repositories
 {
-    public class RegistrationRepository
+    public class RegistrationRepository : IRegistrationRepository
     {
+        private readonly Database.Database _db;
+        public RegistrationRepository(Database.Database db)
+        {
+            _db = db;
+        }
+
+        public List<RegisteredVisitor> GetRegisteredVisitors(int startDate, int endDate)
+        {
+            var data = new List<RegisteredVisitor>();
+            using var dbConn = _db.GetConnection();
+            // using var tx = dbConn.BeginTransaction();
+
+            using var query = new NpgsqlCommand("SELECT visitor_id, name, phone_number, visit_date FROM visitors", dbConn);
+            using var reader = query.ExecuteReader();
+
+            while (reader.Read())
+            {
+                data.Add(new RegisteredVisitor
+                {
+                    Id = reader.GetInt32(0),
+                    Name = reader.GetString(1),
+                    PhoneNumber = reader.GetInt32(2),
+                    Date = reader.GetDateTime(3).ToString("dd-MM-yyyy"),
+                });
+            }
+
+            return data;
+        }
     }
 }
