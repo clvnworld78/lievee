a8664769aa8250c3c363afd41524600cb49b5cda
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-28 00:35:10 +0700

    FEAT: Add DTO for creating a new link (unique code)

---
+++ Services/UniqueCodeService.cs
@@ -0,0 +1,28 @@
+﻿using lievee.Models;
+using lievee.Repositories;
+
+namespace lievee.Services
+{
+    public class UniqueCodeService : IUniqueCodeService
+    {
+        private readonly IUniqueCodeRepository _repo;
+        public UniqueCodeService(IUniqueCodeRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<ServiceResult<string>> GenerateUniqueLink()
+        {
+            Guid uuidv4 = Guid.NewGuid();
+            var uniqueCode = new UniqueCode { Code = uuidv4 };
+            try
+            {
+                await _repo.SaveUniqueCode(uniqueCode);
+                return ServiceResult<string>.Success(uuidv4.ToString());
+            } catch (Exception ex)
+            {
+                return ServiceResult<string>.Failed(ex.Message);
+            }
+        }
+    }
+}
