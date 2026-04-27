3a6d816f2133eb2a3bc8d339552b4e8cc5c2560c
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-27 17:47:33 +0700

    FEAT: Add interface for registration repo

--- Repositories/IRegistrationRepository.cs
+++ Repositories/IRegistrationRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace lievee.Repositories
+﻿using lievee.Models;
+
+namespace lievee.Repositories
 {
     public interface IRegistrationRepository
     {
+        List<RegisteredVisitor> GetRegisteredVisitors(DateOnly startDate, DateOnly endDate);
     }
 }
