86265c7b79cf9459ad241aac443a600175feac7e
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-28 16:42:29 +0700

    FEAT: Add new optional field for getting a link code

--- Models/RegisteredVisitor.cs
+++ Models/RegisteredVisitor.cs
@@ -3,6 +3,7 @@
     public class RegisteredVisitor
     {
         public int Id { get; set; }
+        public int? LinkCode { get; set; }
         public required string Name { get; set; }
         public required int PhoneNumber { get; set; }
         public required string Date { get; set; }
