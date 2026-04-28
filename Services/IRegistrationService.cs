12f2493a1a4c7a656817e86b57bc0f83228ab937
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-28 16:45:31 +0700

    FEAT: Add new interface for saving new visitor

--- Services/IRegistrationService.cs
+++ Services/IRegistrationService.cs
@@ -5,5 +5,6 @@ namespace lievee.Services
     public interface IRegistrationService
     {
         List<RegisteredVisitor> GetRegisteredVisitors(DateOnly startDate, DateOnly endDate);
+        Task<ServiceResultNoData> RegisterVisitorDate(string link, string name, int phoneNumber, DateOnly visitDate);
     }
 }
