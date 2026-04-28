b3bc0e67c3ee6c42281e7588f57cf422d64db2e8
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-28 22:57:01 +0700

    FEAT: Add new DTO for holiday

---
+++ Models/Holiday.cs
@@ -0,0 +1,21 @@
+﻿namespace lievee.Models
+{
+    public class Holiday
+    {
+        public int? HolidayId { get; set; }
+        public int? UserId { get; set; }
+        public string? Username { get; set; }
+        public required DateOnly Date {  get; set; }
+
+
+        public static Holiday NewHoliday(int userId, DateOnly date)
+        {
+            return new Holiday { UserId = userId, Date = date };
+        }
+
+        public static Holiday NewRegisteredHoliday(int holidayId, int userId, string username, DateOnly date)
+        {
+            return new Holiday { HolidayId = holidayId, UserId = userId, Username = username, Date = date };
+        }
+    }
+}
