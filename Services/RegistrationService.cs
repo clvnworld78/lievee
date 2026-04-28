f33611d2ab6b043fef42a0a1476d4a59184e3ede
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-28 21:30:22 +0700

    FEAT: Add new service to delete registered visitor data
    Also fix new visitor service for some basic error handling

--- Services/RegistrationService.cs
+++ Services/RegistrationService.cs
@@ -5,25 +5,46 @@ namespace lievee.Services
 {
     public class RegistrationService : IRegistrationService
     {
-        private readonly IRegistrationRepository _repo;
-        public RegistrationService(IRegistrationRepository repo)
+        private readonly IRegistrationRepository _registRepo;
+        private readonly IUniqueCodeRepository _linkRepo;
+        public RegistrationService(IRegistrationRepository repo, IUniqueCodeRepository repo2)
         {
-            _repo = repo;
+            _registRepo = repo;
+            _linkRepo = repo2;
         }
 
         public List<RegisteredVisitor> GetRegisteredVisitors(DateOnly startDate, DateOnly endDate)
         {
-            return _repo.GetRegisteredVisitors(startDate, endDate);
+            return _registRepo.GetRegisteredVisitors(startDate, endDate);
         }
 
         public async Task<ServiceResultNoData> RegisterVisitorDate(string link, string name, int phoneNumber, DateOnly visitDate)
         {
-            var visitor = new Visitor { LinkCode = link, Name = name, PhoneNumber = phoneNumber, Date = visitDate };
-
             try
             {
-                await _repo.SaveNewVisitor(visitor);
-                return ServiceResultNoData.SuccessNoData();
+                var linkId = await _linkRepo.ResolveLinkIdAsync(link);
+                var visitor = RegisteredVisitor.NewVisitor(linkId, name, phoneNumber, visitDate);
+
+                await _registRepo.SaveNewVisitor(visitor);
+                return ServiceResultNoData.Success();
+            } catch (Exception ex)
+            {
+                return ServiceResultNoData.Failed(ex.Message);
+            }
+        }
+
+        public async Task<ServiceResultNoData> DeleteVisitorData(int visitorId)
+        {
+            try
+            {
+                var registeredVisitor = await _registRepo.ResolveVisitorId(visitorId);
+                if (registeredVisitor == null)
+                {
+                    return ServiceResultNoData.Failed("data not found");
+                }
+
+                await _registRepo.DeleteRegisteredVisitor(registeredVisitor);
+                return ServiceResultNoData.Success();
             } catch (Exception ex)
             {
                 return ServiceResultNoData.Failed(ex.Message);
