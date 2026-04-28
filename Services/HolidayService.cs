b2d011b604e1706cc1f53d909ecdbd2cdbb7d67e
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-28 23:00:16 +0700

    FEAT: Add new service that implement the holiday service interface to add, read, update, and delete holiday records

---
+++ Services/HolidayService.cs
@@ -0,0 +1,60 @@
+﻿using lievee.Models;
+using lievee.Repositories;
+
+namespace lievee.Services
+{
+    public class HolidayService : IHolidayService
+    {
+        private readonly IHolidayRepository _repo;
+        public HolidayService(IHolidayRepository repo) { _repo = repo; }
+
+        public async Task<ServiceResultNoData> CreateNewHoliday(DateOnly date, Users currentUser)
+        {
+            var holiday = Holiday.NewHoliday(currentUser.Id, date);
+
+            try
+            {
+                await _repo.SaveNewHoliday(holiday);
+                return ServiceResultNoData.Success();
+            } catch (Exception ex)
+            {
+                return ServiceResultNoData.Failed(ex.Message);
+            }
+        }
+
+        public async Task<ServiceResult<List<Holiday>>> GetHolidays(DateOnly startDate, DateOnly endDate)
+        {
+            var data = await _repo.GetHoliday(startDate, endDate);
+            if (data == null)
+            {
+                return ServiceResult<List<Holiday>>.Failed("No holiday data found");
+            }
+            
+            return ServiceResult<List<Holiday>>.Success(data);
+        }
+
+        public async Task<ServiceResultNoData> UpdateHoliday(int holidayId, DateOnly newDate)
+        {
+            try
+            {
+                await _repo.UpdateHoliday(holidayId, newDate);
+                return ServiceResultNoData.Success();
+            } catch (Exception ex)
+            {
+                return ServiceResultNoData.Failed(ex.Message);
+            }
+        }
+
+        public async Task<ServiceResultNoData> DeleteHoliday(int holidayId)
+        {
+            try
+            {
+                await _repo.DeleteHoliday(holidayId);
+                return ServiceResultNoData.Success();
+            } catch (Exception ex)
+            {
+                return ServiceResultNoData.Failed(ex.Message);
+            }
+        }
+    }
+}
