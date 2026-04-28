0024d9519c37f04efa132ccd469e664580735ad9
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-28 16:43:47 +0700

    FEAT: Add new DTO when visitor want to register a new visit

---
+++ Models/Visitor.cs
@@ -0,0 +1,10 @@
+﻿namespace lievee.Models
+{
+    public class Visitor
+    {
+        public required string LinkCode { get; set; }
+        public required string Name { get; set; }
+        public required int PhoneNumber { get; set; }
+        public required DateOnly Date { get; set; }
+    }
+}
