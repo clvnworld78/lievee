2eef81c1c674b73e898ddc7e5d5a95f907e062bb
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-28 21:28:08 +0700

    REFRACTOR: Rename static method for consistency

--- Models/ServiceResultNoData.cs
+++ Models/ServiceResultNoData.cs
@@ -5,7 +5,7 @@
         public bool IsSuccess { get; private set; }
         public string? Err { get; private set; }
 
-        public static ServiceResultNoData SuccessNoData() => new ServiceResultNoData { IsSuccess = true };
+        public static ServiceResultNoData Success() => new ServiceResultNoData { IsSuccess = true };
         public static ServiceResultNoData Failed(string msg) => new ServiceResultNoData { IsSuccess = false, Err = msg };
     }
 }
