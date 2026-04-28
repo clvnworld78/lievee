3f0a69832a40dc809dac6d2a9f31bea2271dd0e7
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-28 22:58:17 +0700

    FEAT: Add new interface for saving, fetching, updating, and deleting holiday records

---
+++ Repositories/IHolidayRepository.cs
@@ -0,0 +1,12 @@
+﻿using lievee.Models;
+
+namespace lievee.Repositories
+{
+    public interface IHolidayRepository
+    {
+        Task SaveNewHoliday(Holiday newHoliday);
+        Task<List<Holiday>?> GetHoliday(DateOnly startDate, DateOnly endDate);
+        Task UpdateHoliday(int holidayId, DateOnly newDate);
+        Task DeleteHoliday(int holidayId);
+    }
+}
