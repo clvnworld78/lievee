f87ec9a3808340cfc7a802f73cadfd87725278ec
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-28 22:57:40 +0700

    FEAT: Add new DTO for users

---
+++ Models/Users.cs
@@ -0,0 +1,15 @@
+﻿namespace lievee.Models
+{
+    public class Users
+    {
+        public required int Id { get; set; }
+        public required string Username { get; set; }
+        public required UserRole Role { get; set; }
+    }
+
+    public enum UserRole
+    {
+        Admin,
+        User
+    }
+}
