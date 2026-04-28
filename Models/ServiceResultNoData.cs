6ff894d6fc2b48ee371c7311924e8e173bc9922f
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-28 16:43:10 +0700

    FEAT: Add new return object for when the task return nothing

---
+++ Models/ServiceResultNoData.cs
@@ -0,0 +1,11 @@
+﻿namespace lievee.Models
+{
+    public class ServiceResultNoData
+    {
+        public bool IsSuccess { get; private set; }
+        public string? Err { get; private set; }
+
+        public static ServiceResultNoData SuccessNoData() => new ServiceResultNoData { IsSuccess = true };
+        public static ServiceResultNoData Failed(string msg) => new ServiceResultNoData { IsSuccess = false, Err = msg };
+    }
+}
