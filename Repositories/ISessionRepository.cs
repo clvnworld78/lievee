5c48e1c4a79d81e77e870b6eff0712269cb80be7
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-29 15:15:34 +0700

    FEAT: Add 2 new interface for login process
    The first one is to fetch user data based on username and the second is to save the token on successful login

--- Repositories/ISessionRepository.cs
+++ Repositories/ISessionRepository.cs
@@ -5,5 +5,7 @@ namespace lievee.Repositories
     public interface ISessionRepository
     {
         Task<Users> GetUserAsync(string token);
+        Task<Users> GetUserCredentialAsync(string username);
+        Task SaveToken(Guid token, long userId);
     }
 }
