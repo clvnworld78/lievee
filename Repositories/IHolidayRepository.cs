47c529fa1aba5cbbc9aab3c9384d1ae779a0eea1
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-29 12:49:36 +0700

    REFRACTOR: Remove nullable return type for better readability more maintainable code

--- Repositories/IHolidayRepository.cs
+++ Repositories/IHolidayRepository.cs
@@ -5,7 +5,7 @@ namespace lievee.Repositories
     public interface IHolidayRepository
     {
         Task SaveNewHoliday(Holiday newHoliday);
-        Task<List<Holiday>?> GetHoliday(DateOnly startDate, DateOnly endDate);
+        Task<List<Holiday>> GetHoliday(DateOnly startDate, DateOnly endDate);
         Task UpdateHoliday(int holidayId, DateOnly newDate);
         Task DeleteHoliday(int holidayId);
     }
