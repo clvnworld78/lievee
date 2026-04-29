da4ec5abc832e9845c93d94ee71e240782c40208
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-29 15:14:17 +0700

    FIX: Fix incorrect data type to match db type

--- Models/Users.cs
+++ Models/Users.cs
@@ -2,21 +2,27 @@
 {
     public class Users
     {
-        public required int Id { get; set; }
+        public required long Id { get; set; }
         public required string Username { get; set; }
+        public required byte[] Password { get; set; }
         public required UserRole Role { get; set; }
         public required bool Valid { get; set; }
 
         private Users() { }
 
-        public static Users NewAuthenticatedUser(int id, string username, UserRole role)
+        public static Users NewAuthenticatedUser(long id, string username, byte[] pass, UserRole role)
         {
-            return new Users { Id = id, Username = username, Role = role, Valid = true };
+            return new Users { Id = id, Username = username, Password = pass, Role = role, Valid = true };
         }
 
         public static Users NewInvalidUser()
         {
-            return new Users { Id = 0, Username = "", Role = UserRole.User, Valid = false };
+            return new Users { Id = 0, Username = "", Password = [], Role = UserRole.User, Valid = false };
+        }
+
+        public static Users NewLogin(long id, string username, byte[] hashedPass, UserRole role)
+        {
+            return new Users { Id = id, Username = username, Password = hashedPass, Role = role, Valid = true };
         }
     }
 
