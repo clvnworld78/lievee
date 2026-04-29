375403949b8f380d9747d631d67a3dde78fea942
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-29 15:13:17 +0700

    FIX: Fix incorrect database credential

--- Database/Database.cs
+++ Database/Database.cs
@@ -4,7 +4,7 @@
     public class Database
     {
         private static readonly Database _pool = new();
-        private const string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=test123;Database=lievee;Pooling=true;MinPoolSize=2;MaxPoolSize=10";
+        private const string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=hahaha123;Database=lievee;Pooling=true;MinPoolSize=2;MaxPoolSize=10";
         // empty constructor to reinforce singleton (public by default)
         private Database() { }
         public static Database Pool => _pool;
