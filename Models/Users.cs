5c951c095087789458021168d151ae8408e9772a
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-29 09:51:01 +0700

    FIX: Fix possible null reference warning by providing default value instead

--- Models/Users.cs
+++ Models/Users.cs
@@ -5,6 +5,19 @@
         public required int Id { get; set; }
         public required string Username { get; set; }
         public required UserRole Role { get; set; }
+        public required bool Valid { get; set; }
+
+        private Users() { }
+
+        public static Users NewAuthenticatedUser(int id, string username, UserRole role)
+        {
+            return new Users { Id = id, Username = username, Role = role, Valid = true };
+        }
+
+        public static Users NewInvalidUser()
+        {
+            return new Users { Id = 0, Username = "", Role = UserRole.User, Valid = false };
+        }
     }
 
     public enum UserRole
