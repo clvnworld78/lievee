4ef2f2085f95239e9984eb2847769eae2237fee7
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-29 09:53:55 +0700

    FEAT: Add service to authenticate user from token

---
+++ Services/SessionService.cs
@@ -0,0 +1,28 @@
+﻿using lievee.Models;
+using lievee.Repositories;
+
+namespace lievee.Services
+{
+    public class SessionService : ISessionService
+    {
+        private readonly ISessionRepository _repo;
+        public SessionService(ISessionRepository repo) { _repo = repo; }
+
+        public async Task<ServiceResult<Users>> AuthenticateUserAsync(string token)
+        {
+            try
+            {
+                var user = await _repo.GetUserAsync(token);
+                if (!user.Valid)
+                {
+                    return ServiceResult<Users>.Failed("token is invalid or expired");
+                }
+
+                return ServiceResult<Users>.Success(user);
+            } catch (Exception ex)
+            {
+                return ServiceResult<Users>.Failed(ex.Message);
+            }
+        }
+    }
+}
