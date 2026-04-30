0fb5f03e2da21ba75fcf9536b81bf54a25f342b6
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-30 15:27:31 +0700

    FIX: Fix incorrect data type for token from string to Guid
    DB use native UUID type

--- Services/ISessionService.cs
+++ Services/ISessionService.cs
@@ -4,7 +4,7 @@ namespace lievee.Services
 {
     public interface ISessionService
     {
-        Task<ServiceResult<Users>> AuthenticateUserAsync(string token);
+        Task<ServiceResult<Users>> AuthenticateUserAsync(Guid token);
         Task<ServiceResult<Guid>> LoginAsync(string username, string password);
     }
 }
