b183b0d47e6478fd2b4dc1829412eaf0e12bb563
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-30 15:30:46 +0700

    FEAT: Add service to add new users both admin and user role

---
+++ Services/UserService.cs
@@ -0,0 +1,49 @@
+﻿using lievee.Helper;
+using lievee.Models;
+using lievee.Repositories;
+
+namespace lievee.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _repo;
+        public UserService(IUserRepository repo) { _repo = repo; }
+
+        public async Task<ServiceResult<long>> CreateNewAdminUserAsync(string username, string password)
+        {
+            var hashedPassword = PasswordHasher.HashPassword(password);
+            try
+            {
+                var userId = await _repo.SaveNewUserAsync(username, hashedPassword, UserRole.admin);
+                if (userId == 0)
+                {
+                    return ServiceResult<long>.Failed("repo failed saving a new user", 500);
+                }
+
+                return ServiceResult<long>.Success(userId);
+            } catch (Exception ex)
+            {
+                return ServiceResult<long>.Failed(ex.Message, 500);
+            }
+        }
+
+        public async Task<ServiceResult<long>> CreateNewUserUserAsync(string username, string password)
+        {
+            var hashedPassword = PasswordHasher.HashPassword(password);
+            try
+            {
+                var userId = await _repo.SaveNewUserAsync(username, hashedPassword, UserRole.user);
+                if (userId == 0)
+                {
+                    return ServiceResult<long>.Failed("repo failed saving a new user", 500);
+                }
+
+                return ServiceResult<long>.Success(userId);
+            }
+            catch (Exception ex)
+            {
+                return ServiceResult<long>.Failed(ex.Message, 500);
+            }
+        }
+    }
+}
