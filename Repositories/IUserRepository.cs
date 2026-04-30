baf7f6fd3f61161898a874e016f6094dc197e5ed
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-30 15:32:46 +0700

    FEAT: Add new repo to save new user

---
+++ Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using lievee.Models;
+
+namespace lievee.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<long> SaveNewUserAsync(string username, byte[] password, UserRole role);
+    }
+}
