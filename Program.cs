4f742a6e1f271d00c457ac9d676e0b1127dbe45c
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-30 15:39:59 +0700

    FEAT: Add authentication middleware config

--- Program.cs
+++ Program.cs
@@ -2,6 +2,10 @@ using lievee.Database;
 using lievee.Middleware;
 using lievee.Repositories;
 using lievee.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.OpenApi.Models;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +14,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("cookie", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Cookie,
+        Name = "token",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "cookie"
+                }
+            },
+            new string[] { }
+        }
+    });
+});
 
 builder.Services.AddSingleton(Database.Pool);
 builder.Services.AddScoped<IRegistrationService, RegistrationService>();
@@ -25,13 +52,48 @@ builder.Services.AddScoped<IHolidayService, HolidayService>();
 builder.Services.AddScoped<ISessionRepository, SessionRepository>();
 builder.Services.AddScoped<ISessionService, SessionService>();
 
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 builder.Services.AddHttpContextAccessor();
 
+// so authorization in property can
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.Cookie.Name = "token";
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SameSite = SameSiteMode.Lax;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.None;
+
+        options.Events.OnRedirectToLogin = context =>
+        {
+            if (!context.Response.HasStarted)
+            {
+                context.Response.StatusCode = 401;
+                context.Response.ContentType = "application/json";
+                var response = new { message = "missing token in cookie" };
+                return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+            }
+            return Task.CompletedTask;
+        };
+
+        options.Events.OnRedirectToAccessDenied = context =>
+        {
+            if (!context.Response.HasStarted)
+            {
+                context.Response.StatusCode = 403;
+                context.Response.ContentType = "application/json";
+                var response = new { message = "user does not have the right privilege" };
+                return context.Response.WriteAsync(JsonSerializer.Serialize(response));
+            }
+            return Task.CompletedTask;
+        };
+    });
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
-
-// auth middleware
-app.UseMiddleware<Authentication>();
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -40,7 +102,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+
+// auth middleware
+app.UseMiddleware<Authentication>();
 
 app.UseAuthorization();
 
