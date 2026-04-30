d331aa19e31ac7def7b584f5d348dfe235001169
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-30 15:23:54 +0700

    FEAT: Add helper function so it can be used across service that needs it

---
+++ Helper/PasswordHasher.cs
@@ -0,0 +1,20 @@
+﻿using lievee.Models;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lievee.Helper
+{
+    public static class PasswordHasher
+    {
+        public static byte[] HashPassword(string password)
+        {
+            var inputByte = Encoding.UTF8.GetBytes(password);
+            return SHA256.HashData(inputByte);
+        }
+
+        public static bool VerifyPassword(string password, byte[] hashedPassword)
+        {
+            return hashedPassword.SequenceEqual(PasswordHasher.HashPassword(password));
+        }
+    }
+}
