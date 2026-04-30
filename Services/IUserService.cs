3a3feaa7b4b2491018a8a8ec52f8dde3bc76dd06
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-30 15:27:57 +0700

    FEAT: Add interface for creating admin and user role users

---
+++ Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using lievee.Models;
+
+namespace lievee.Services
+{
+    public interface IUserService
+    {
+        Task<ServiceResult<long>> CreateNewAdminUserAsync(string username, string password);
+        Task<ServiceResult<long>> CreateNewUserUserAsync(string username, string password);
+    }
+}
