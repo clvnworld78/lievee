946ffb31724504968014c8f2a79e73208147b827
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-30 15:39:31 +0700

    FIX: Fix incorrect data type for token from string to Guid and int to string for phone number

--- Models/Visitor.cs
+++ Models/Visitor.cs
@@ -2,9 +2,9 @@
 {
     public class Visitor
     {
-        public required string LinkCode { get; set; }
+        public required Guid LinkCode { get; set; }
         public required string Name { get; set; }
-        public required int PhoneNumber { get; set; }
+        public required string PhoneNumber { get; set; }
         public required DateOnly Date { get; set; }
     }
 }
