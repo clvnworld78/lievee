63b736e4ad0f34d4fb6b3f62e65a9d76eace0966
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-30 15:38:27 +0700

    FIX: Fix incorrect data type for id from int to long
    DB use bigint for id

--- Models/Holiday.cs
+++ Models/Holiday.cs
@@ -2,18 +2,18 @@
 {
     public class Holiday
     {
-        public int? HolidayId { get; set; }
-        public int? UserId { get; set; }
+        public long? HolidayId { get; set; }
+        public long? UserId { get; set; }
         public string? Username { get; set; }
         public required DateOnly Date {  get; set; }
 
 
-        public static Holiday NewHoliday(int userId, DateOnly date)
+        public static Holiday NewHoliday(long userId, DateOnly date)
         {
             return new Holiday { UserId = userId, Date = date };
         }
 
-        public static Holiday NewRegisteredHoliday(int holidayId, int userId, string username, DateOnly date)
+        public static Holiday NewRegisteredHoliday(long holidayId, long userId, string username, DateOnly date)
         {
             return new Holiday { HolidayId = holidayId, UserId = userId, Username = username, Date = date };
         }
--- Models/RegisteredVisitor.cs
+++ Models/RegisteredVisitor.cs
@@ -2,18 +2,18 @@
 {
     public class RegisteredVisitor
     {
-        public int? VisitorId { get; set; }
-        public required int LinkId { get; set; }
+        public long? VisitorId { get; set; }
+        public required long LinkId { get; set; }
         public required string Name { get; set; }
-        public required int PhoneNumber { get; set; }
+        public required string PhoneNumber { get; set; }
         public required DateOnly Date { get; set; }
 
-        public static RegisteredVisitor NewVisitor(int linkId, string name, int phoneNumber, DateOnly visitDate)
+        public static RegisteredVisitor NewVisitor(long linkId, string name, string phoneNumber, DateOnly visitDate)
         {
             return new RegisteredVisitor { LinkId = linkId, Name = name, PhoneNumber = phoneNumber, Date = visitDate };
         }
 
-        public static RegisteredVisitor NewRegisteredVisitor(int visitorId, int linkId, string name, int phoneNumber, DateOnly visitDate)
+        public static RegisteredVisitor NewRegisteredVisitor(long visitorId, long linkId, string name, string phoneNumber, DateOnly visitDate)
         {
             return new RegisteredVisitor { VisitorId = visitorId, LinkId = linkId, Name = name, PhoneNumber = phoneNumber, Date = visitDate };
         }
