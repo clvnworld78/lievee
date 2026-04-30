462f392f49dba35f940560806a1654361b684cb5
Calvin C. Haris <calvincaesar777@gmail.com>
2026-04-30 15:26:54 +0700

    FIX: Fix incorrect data type for id from int to long
    DB use bigint for id

--- Services/IRegistrationService.cs
+++ Services/IRegistrationService.cs
@@ -4,8 +4,8 @@ namespace lievee.Services
 {
     public interface IRegistrationService
     {
-        List<RegisteredVisitor> GetRegisteredVisitors(DateOnly startDate, DateOnly endDate);
-        Task<ServiceResultNoData> RegisterVisitorDate(string link, string name, int phoneNumber, DateOnly visitDate);
+        Task<ServiceResult<List<RegisteredVisitor>>> GetRegisteredVisitors(DateOnly startDate, DateOnly endDate);
+        Task<ServiceResultNoData> RegisterVisitorDate(Guid link, string name, string phoneNumber, DateOnly visitDate);
         Task<ServiceResultNoData> DeleteVisitorData(int visitorId);
     }
 }
